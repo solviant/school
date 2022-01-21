712088b15a2d7abbcf3367aa29b95f3dd4fff606
solviant <driad@o2.pl>
2022-01-21 16:19:03 +0100

    First commit

---
+++ schoolregister/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    birth_date = models.DateField()
+
+class Semester(models.Model):
+    year = models.SmallIntegerField()
+    number = models.SmallIntegerField(choices=[(1, 1), (2, 2)])
+
+class Subject(models.Model):
+    name = models.CharField(max_length=50)
+
+class Teacher(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    subjects = models.ManyToManyField('Subject')
+
+class SchoolClass:
+    name = models.CharField(max_length=2)
+    semester = models.ForeignKey('Semester')
+    students = models.ManyToManyField('Student')
+    subjects = models.ManyToManyField('Subject')
+    tutor = models.ForeignKey('Teacher')
