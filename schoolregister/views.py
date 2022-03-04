6713c58190a9f4af27def375422f9cf55c787b02
solviant <driad@o2.pl>
2022-03-04 14:48:39 +0100

    22.03.04 friday afternoon commit

--- schoolregister/views.py
+++ schoolregister/views.py
@@ -55,26 +55,65 @@ def class_students(request, school_class_id):
         'school_class': school_class, 'students': students})
 
 
-# !! Trzeba sprawdzic, czy zwykly uczen po wpisaniu adresu moze sie dostac.
 @login_required
 def class_detail(request, school_class_id, subject_id):
-    school_class = get_object_or_404(SchoolClass, id=school_class_id)
-    # subject = get_object_or_404(Subject, id=subject_id)
+    school_class = get_object_or_404(SchoolClass, id=1) # id=school_class_id
     semesters = school_class.school_year.semester_set.all()
-    students = Student.objects.all() # czy tu <class>_set.all po school_class?
-    students_list = []
-    grades = Grade.objects.all() # czy tu <class>_set.all po school_class?
-    subject = Subject.objects.get(id=subject_id) # !! <Subject.objects.get(pk=1)> To jest do zmiany, na razie wpisane na twardo jako test - przyklad.
-    grades_list_1st = []
-    grades_list_2nd = []
-    uczniowie_lista = []
-    grades_list = []
+    students = school_class.students.all()
+    # co jesli student zostal przeniesiony?
+    grades_1_sem = []
+    grades_2_sem = []
+    grades = Grade.objects.all() # to zmienic - jest tu jedynie do max_count_grades, powinno dac sie inaczej
+
+    subject = Subject.objects.get(id=1) # id=subject_id
     # students_grades = [{'students': uczniowie_lista}, {'grades': []}]
     x = {'Wozniak': [1, 2, 3]}
-    max_count_grades_1st = 0
-    max_count_grades_2nd = 0
+    max_count_grades_1_sem = 0
+    max_count_grades_2_sem = 0
     semesters_len = len(semesters)
     i = -1
+
+    empty_spaces_1_sem = [] # !! check all inst
+    to_max_grades = ''
+
+    """Depending of accepted choice - add proper logic description"""
+
+    for sc_student in school_class.students.all():
+        # sprawdzic mozliwosc uzycia "entry_set"
+        for semester in semesters:
+            if semester.number == 1:
+                grades_count_1_sem = len(grades.filter(school_year_id=school_class.school_year.id, semester_id=semester.id, subject_id=subject.id, student_id=sc_student.id))
+                if max_count_grades_1_sem < grades_count_1_sem:
+                    max_count_grades_1_sem = grades_count_1_sem
+                diff = max_count_grades_1_sem - grades_count_1_sem
+            # if diff > 0:    
+                for empty in range(0, diff):
+                    to_max_grades += '1'
+                empty_spaces_1_sem.append(to_max_grades)
+            elif semester.number == 2:
+                grades_count_2_sem = len(grades.filter(school_year_id=school_class.school_year.id, semester_id=semester.id, subject_id=subject.id, student_id=sc_student.id))
+                if max_count_grades_2_sem < grades_count_2_sem:
+                    max_count_grades_2_sem = grades_count_2_sem
+
+
+    col_span_1_sem = max_count_grades_1_sem + 3
+    col_span_2_sem = max_count_grades_2_sem + 3
+    col_span_year = col_span_1_sem + col_span_2_sem
+
+    for student in students:
+        grades_1_sem.append(student.grade_set.filter(school_year=school_class.school_year, semester=Semester.objects.get(number=1)))
+        grades_2_sem.append(student.grade_set.filter(school_year=school_class.school_year, semester=Semester.objects.get(number=2)))
+
+    step_02 = -1
+
+    """for grade in grades_1_sem:
+        difference = max_count_grades_1_sem - len(grade)
+        step_02 += 1
+        for fill_grades in range(1, step_02):
+            grades_1_sem[step_02].append('o')"""
+
+    students_grades_1_sem = list(zip(students, grades_1_sem, empty_spaces_1_sem))
+    students_grades_2_sem = list(zip(students, grades_2_sem))
 
     # for sc_student in school_class.students.all():
     #     students_list.append(sc_student)
@@ -95,56 +134,27 @@ def class_detail(request, school_class_id, subject_id):
         # grade_form.subject = subject  # To nie działa
 
 
-    # w petli najpierw powinien byc semester
-
-    for sc_student in school_class.students.all():
-        students_list.append(sc_student)
-        grades_list.append(sc_student.grade_set.filter(school_year_id=school_class.school_year.id, semester_id=1, subject_id=1, student_id=sc_student.id))
-        # sprawdzic mozliwosc uzycia "entry_set"
-        for semester in semesters:
-            if semester.number == 1:
-                grades_count_1st = len(grades.filter(school_year_id=school_class.school_year.id, semester_id=semester.id, subject_id=subject.id, student_id=sc_student.id))
-                if max_count_grades_1st < grades_count_1st:
-                    max_count_grades_1st = grades_count_1st
-                
-                
-                
-    for sc_student in school_class.students.all():
-        for semester in semesters:
-            if semester.number == 1:            
-                for grade in grades:
-                    if grade.student == sc_student and grade.subject == subject and grade.semester == semester:
-                        grades_list_1st.append(grade)
-                        # if student_grades[sc_student.full_name]:
-                        # i += 1
-                        # uczniowie_lista.append(sc_student)
-            elif semester.number == 2:
-                grades_count_2nd = len(grades.filter(school_year_id=school_class.school_year.id, semester_id=semester.id, subject_id=subject.id, student_id=sc_student.id))
-                if max_count_grades_2nd < grades_count_2nd:
-                    max_count_grades_2nd = grades_count_2nd
-                for grade in grades:
-                    if grade.student == sc_student and grade.subject == subject and grade.semester == semester:
-                        grades_list_2nd.append(grade)
-                        max_count_grades_2nd += 1
-    
-    col_span_1_sem = max_count_grades_1st + 3
-    col_span_2_sem = max_count_grades_2nd + 3
-    col_span_year = col_span_1_sem + col_span_2_sem
-
     return render(request, 'schoolregister/nauczyciel/klasa.html', {
         'school_class': school_class, 'students': students, 
         'grades': grades, 'subject': subject, 'grade_form': grade_form,
-        'students_list': students_list,
-        'grades_list': grades_list,
-        'grades_list_1st': grades_list_1st,
-        'grades_list_2nd': grades_list_2nd,
-        'max_count_grades_1st': max_count_grades_1st,
-        'max_count_grades_2nd': max_count_grades_2nd, 
-        'col_span_1_sem': col_span_1_sem,
-        'col_span_2_sem': col_span_2_sem,
-        'col_span_year': col_span_year,
-        'semesters': semesters,
-        'semesters_len': semesters_len,})
+        'students': students, # czy to potrzebne?
+        'students_grades_1_sem': students_grades_1_sem,
+        'students_grades_2_sem': students_grades_2_sem,
+        'max_count_grades_1_sem': max_count_grades_1_sem,
+        'max_count_grades_2_sem': max_count_grades_2_sem, 
+        'col_span_1_sem': col_span_1_sem, # czy to potrzebne?
+        'col_span_2_sem': col_span_2_sem, # czy to potrzebne?
+        'col_span_year': col_span_year, # czy to potrzebne?
+        'semesters': semesters, # czy to potrzebne?
+        'semesters_len': semesters_len, # czy to potrzebne?
+        })
+
+
+def edit_grade(request):
+    if request.method=='GET':
+        grade_to_edit = request.GET.get['ocena']
+
+
 
 
 @login_required
@@ -202,3 +212,99 @@ def test_site03(request):
 
 def test_site04(request):
     return render(request, 'schoolregister/test_site04.html')
+
+
+
+# Graveyard:
+
+"""Old code - may redundancies probably"""
+"""
+# !! Trzeba sprawdzic, czy zwykly uczen po wpisaniu adresu moze sie dostac.
+@login_required
+def class_detail(request, school_class_id, subject_id):
+    # subject = get_object_or_404(Subject, id=subject_id)
+    school_class = get_object_or_404(SchoolClass, id=1) # id=school_class_id
+    semesters = school_class.school_year.semester_set.all()
+    students = Student.objects.all() # czy tu <class>_set.all po school_class?
+    students_list = []
+    grades = Grade.objects.all() # czy tu <class>_set.all po school_class?
+    subject = Subject.objects.get(id=1) # id=subject_id
+    grades_list_1st = []
+    grades_list_2nd = []
+    uczniowie_lista = []
+    grades_list = []
+    # students_grades = [{'students': uczniowie_lista}, {'grades': []}]
+    x = {'Wozniak': [1, 2, 3]}
+    max_count_grades_1st = 0
+    max_count_grades_2nd = 0
+    semesters_len = len(semesters)
+    i = -1
+
+    # for sc_student in school_class.students.all():
+    #     students_list.append(sc_student)
+    #     grades_list.append(sc_student.grade_set.filter(school_year_id=school_class.school_year.id, semester_id=1, subject_id=1, student_id=sc_student.id))
+    # students_grades = zip(students_list, grades_list)
+    # [[students], [[scs_grades],[],...]]
+
+    if request.method == 'POST':
+        grade_form = GradeForm(request.POST)
+        # grade_form.subject = subject  # To nie działa
+        if grade_form.is_valid():
+            grade_form.save()
+            return HttpResponseRedirect('')
+        # else:
+        #     return HttpResponse("Error.")
+    else:
+        grade_form = GradeForm()
+        # grade_form.subject = subject  # To nie działa
+
+
+    # w petli najpierw powinien byc semester
+
+    for sc_student in school_class.students.all():
+        students_list.append(sc_student)
+        grades_list.append(sc_student.grade_set.filter(school_year_id=school_class.school_year.id, semester_id=1, subject_id=1, student_id=sc_student.id))
+        # sprawdzic mozliwosc uzycia "entry_set"
+        for semester in semesters:
+            if semester.number == 1:
+                grades_count_1st = len(grades.filter(school_year_id=school_class.school_year.id, semester_id=semester.id, subject_id=subject.id, student_id=sc_student.id))
+                if max_count_grades_1st < grades_count_1st:
+                    max_count_grades_1st = grades_count_1st
+                        
+    for sc_student in school_class.students.all():
+        for semester in semesters:
+            if semester.number == 1:            
+                for grade in grades:
+                    if grade.student == sc_student and grade.subject == subject and grade.semester == semester:
+                        grades_list_1st.append(grade)
+                        # if student_grades[sc_student.full_name]:
+                        # i += 1
+                        # uczniowie_lista.append(sc_student)
+            elif semester.number == 2:
+                grades_count_2nd = len(grades.filter(school_year_id=school_class.school_year.id, semester_id=semester.id, subject_id=subject.id, student_id=sc_student.id))
+                if max_count_grades_2nd < grades_count_2nd:
+                    max_count_grades_2nd = grades_count_2nd
+                for grade in grades:
+                    if grade.student == sc_student and grade.subject == subject and grade.semester == semester:
+                        grades_list_2nd.append(grade)
+                        max_count_grades_2nd += 1
+    
+    col_span_1_sem = max_count_grades_1st + 3
+    col_span_2_sem = max_count_grades_2nd + 3
+    col_span_year = col_span_1_sem + col_span_2_sem
+
+    return render(request, 'schoolregister/nauczyciel/klasa.html', {
+        'school_class': school_class, 'students': students, 
+        'grades': grades, 'subject': subject, 'grade_form': grade_form,
+        'students_list': students_list,
+        'grades_list': grades_list,
+        'grades_list_1st': grades_list_1st,
+        'grades_list_2nd': grades_list_2nd,
+        'max_count_grades_1st': max_count_grades_1st,
+        'max_count_grades_2nd': max_count_grades_2nd, 
+        'col_span_1_sem': col_span_1_sem,
+        'col_span_2_sem': col_span_2_sem,
+        'col_span_year': col_span_year,
+        'semesters': semesters,
+        'semesters_len': semesters_len,})
+"""
